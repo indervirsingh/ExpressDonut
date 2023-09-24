514f83899655a3425a8f961a4712a3b507a4e0ed
Indervir Singh <iindervirsingh@gmail.com>
2023-09-24 06:30:37 -0700

    updating Food model

--- frontend/src/app/shared/models/Food.ts
+++ frontend/src/app/shared/models/Food.ts
@@ -2,9 +2,8 @@ export class Food {
     id!: string
     name!: string
     price?: number
-    tags?: string[]
     favorite!: boolean
     stars!: number
     imageUrl!: string
-    origins!: string[]
+    tags?: string[]
 }
