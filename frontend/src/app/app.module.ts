a8897a6607732674d81134ab59cb7e3ce4bbde2e
Indervir Singh <iindervirsingh@gmail.com>
2023-09-24 08:13:49 -0700

    updating

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/partials/header/header.component';
-import { HomeComponent } from './components/pages/home/home.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './components/partials/header/header.component'
+import { HomeComponent } from './components/pages/home/home.component'
+import { StarRatingComponent } from './components/partials/star-rating/star-rating.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    StarRatingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
