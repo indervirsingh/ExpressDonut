4a7492eac491246877c56de592f3c659b9ea0b5d
Indervir Singh <iindervirsingh@gmail.com>
2023-09-24 10:23:28 -0700

    linking components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,7 +7,8 @@ import { HeaderComponent } from './components/partials/header/header.component'
 import { HomeComponent } from './components/pages/home/home.component'
 import { StarRatingComponent } from './components/partials/star-rating/star-rating.component'
 import { SearchComponent } from './components/partials/search/search.component'
-import { TagsComponent } from './components/partials/tags/tags.component'
+import { TagsComponent } from './components/partials/tags/tags.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component'
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { TagsComponent } from './components/partials/tags/tags.component'
     HomeComponent,
     StarRatingComponent,
     SearchComponent,
-    TagsComponent
+    TagsComponent,
+    FoodPageComponent
   ],
   imports: [
     BrowserModule,
