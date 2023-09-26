37f957f9398d2ac233e3f921a02c95936ad0a9a2
Indervir Singh <iindervirsingh@gmail.com>
2023-09-26 14:13:54 -0700

    implemented food service api into backend routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import { foods } from './data'
+import { foods, tags } from './data'
 
 const app = express()
 
@@ -9,6 +9,7 @@ app.use(cors({
     origin: ['http://localhost:4200']
 }))
 
+// Routes
 app.get('/api/foods', (req, res) => {
     res.send(foods)
 })
@@ -17,6 +18,22 @@ app.get('/api/foods/search/:searchTerm', (req, res) => {
     const searchTerm = req.params.searchTerm
     const foodsArray = foods.filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()))
     res.send(foodsArray)
+})
+
+app.get('/api/foods/tags', (req, res) => {
+    res.send(tags)
+})
+
+app.get('/api/foods/tag/:tagName', (req, res) => {
+    const tagName = req.params.tagName
+    const foodsArray = foods.filter(food => food.tags?.includes(tagName))
+    res.send(foodsArray)
+})
+
+app.get('/api/foods/:foodId', (req, res) => {
+    const foodId = req.params.foodId
+    const food = foods.find(food => food.id == foodId)
+    res.send(food)
 })
 
 const port = 5000
