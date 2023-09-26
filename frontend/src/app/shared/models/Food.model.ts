6024aa6b4e2ef1508eb59b753af7ce97d38c5577
Indervir Singh <iindervirsingh@gmail.com>
2023-09-26 10:30:57 -0700

    changed price property to required

--- frontend/src/app/shared/models/Food.model.ts
+++ frontend/src/app/shared/models/Food.model.ts
@@ -1,7 +1,7 @@
 export class Food {
     id!: string
     name!: string
-    price?: number
+    price!: number
     favorite!: boolean
     stars!: number
     imageUrl!: string
--- frontend/src/app/shared/models/cart-item.model.ts
+++ frontend/src/app/shared/models/cart-item.model.ts
@@ -4,5 +4,5 @@ export class CartItem {
     constructor (public food: Food) { }
 
     quantity: number = 1
-    price?: number = this.food.price
+    price: number = this.food.price
 }
