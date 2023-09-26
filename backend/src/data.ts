d510dc0af82afb507371adede5ff99f277675d31
Indervir Singh <iindervirsingh@gmail.com>
2023-09-26 15:43:26 -0700

    adding sample users

--- backend/src/data.ts
+++ backend/src/data.ts
@@ -141,3 +141,20 @@ export const tags:any[] = [
     { name: 'Donut Holes', count: 1 },
     { name: 'Dozen', count: 1 },
 ]
+
+export const sample_users: any[] = [
+    {
+        name: "John Doe",
+        email: "john@gmail.com",
+        password: "12345",
+        address: "Toronto",
+        isAdmin: true
+    },
+    {
+        name: "Jane Doe",
+        email: "jane@gmail.com",
+        password: "12345",
+        address: "California",
+        isAdmin: false
+    }
+]
