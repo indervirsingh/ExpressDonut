a6d7679205e423de1d2de9a91f5e5ca08aad546c
Indervir Singh <iindervirsingh@gmail.com>
2023-09-26 10:27:02 -0700

    implemented addToCart and removeFromCart methods

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Cart } from '../shared/models/cart.model';
+import { BehaviorSubject } from 'rxjs';
+import { Food } from '../shared/models/food.model';
+import { CartItem } from '../shared/models/cart-item.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-
+  private cart: Cart = new Cart()
+  private cartSubject: BehaviorSubject<Cart> = new BehaviorSubject(this.cart)
+  
   constructor() { }
+
+  addToCart(food: Food):void {
+    let cartItem = this.cart.items.find(item => item.food.id === food.id)
+
+    if (cartItem) return
+    else {
+      this.cart.items.push(new CartItem(food))
+    }
+  }
+
+  removeFromCart(foodId: string):void {
+    this.cart.items = this.cart.items.filter(item => item.food.id != foodId)
+  }
+
+  
 }
