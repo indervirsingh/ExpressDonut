490acffe2d025dfeaeeb269c07509ee55fa7c405
Indervir Singh <iindervirsingh@gmail.com>
2023-09-26 10:47:32 -0700

    implemented addToCart method

--- frontend/src/app/components/pages/food-page/food-page.component.ts
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core'
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Router } from '@angular/router'
 import { FoodService } from 'src/app/services/food.service'
 import { Food } from 'src/app/shared/models/food.model'
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-food-page',
@@ -12,7 +13,7 @@ export class FoodPageComponent implements OnInit {
 
   food!: Food
 
-  constructor(activatedRoute: ActivatedRoute, foodService: FoodService) {
+  constructor(activatedRoute: ActivatedRoute, foodService: FoodService, private cartService: CartService, private router: Router) {
     activatedRoute.params.subscribe((params) => {
       if (params['id']) {
         this.food = foodService.getFoodById(params['id'])
@@ -23,4 +24,8 @@ export class FoodPageComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  addToCart() {
+    this.cartService.addToCart(this.food)
+    this.router.navigateByUrl('/cart-page')
+  }
 }
