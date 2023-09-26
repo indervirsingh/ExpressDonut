f9fae89653e2b418adb4353cc1ff1454a7e7f24a
Indervir Singh <iindervirsingh@gmail.com>
2023-09-26 13:48:01 -0700

    copied from frontend, removed model imports and changed array types to any for now

---
+++ backend/src/data.ts
@@ -0,0 +1,143 @@
+
+export const foods: any[] = [
+    {
+        id: '1',
+        name: 'Apple Fritter',
+        price: 3.00,
+        favorite: false,
+        stars: 5.0,
+        imageUrl: 'assets/apple_fritter.jpg',
+        tags: ['Fancy', 'Apple']
+    },
+    {
+        id: '2',
+        name: 'Apple & Cherry Turnover',
+        price: 3.00,
+        favorite: false,
+        stars: 5.0,
+        imageUrl: 'assets/apple-cherry_turnovers.jpg',
+        tags: ['Fancy', 'Apple', 'Cherry']
+    },
+    {
+        id: '3',
+        name: 'Cheese Danish',
+        price: 3.00,
+        favorite: false,
+        stars: 5.0,
+        imageUrl: 'assets/cheese_danishes.jpg',
+        tags: ['Fancy', 'Cheese', 'Danish']
+    },
+    {
+        id: '4',
+        name: 'Plain Croissant',
+        price: 3.00,
+        favorite: false,
+        stars: 5.0,
+        imageUrl: 'assets/croissants.jpg',
+        tags: ['Fancy', 'Plain']
+    },
+    {
+        id: '5',
+        name: 'Chocolate Bar',
+        price: 2.00,
+        favorite: false,
+        stars: 5.0,
+        imageUrl: 'assets/chocolate_bars.jpg',
+        tags: ['Bar', 'Chocolate']
+    },
+    {
+        id: '6',
+        name: 'Maple Bar',
+        price: 2.00,
+        favorite: false,
+        stars: 5.0,
+        imageUrl: 'assets/maple_bars.jpg',
+        tags: ['Bar', 'Maple']
+    },
+    {
+        id: '7',
+        name: 'Glazed Twist',
+        price: 2.00,
+        favorite: false,
+        stars: 5.0,
+        imageUrl: 'assets/glazed_twists.jpg',
+        tags: ['Twist', 'Glazed']
+    },
+    {
+        id: '8',
+        name: 'Maple Twist',
+        price: 2.00,
+        favorite: false,
+        stars: 5.0,
+        imageUrl: 'assets/maple-chocolate_twists.jpg',
+        tags: ['Twist', 'Maple']
+    },
+    {
+        id: '9',
+        name: 'Chocolate Twist',
+        price: 2.00,
+        favorite: false,
+        stars: 5.0,
+        imageUrl: 'assets/maple-chocolate_twists.jpg',
+        tags: ['Twist', 'Chocolate']
+    },
+    {
+        id: '10',
+        name: 'French Curler',
+        price: 2.50,
+        favorite: false,
+        stars: 5.0,
+        imageUrl: 'assets/french_donuts.jpg',
+        tags: ['French', 'Chocolate', 'Glazed']
+    },
+    {
+        id: '11',
+        name: 'Devils Food Donut',
+        price: 1.50,
+        favorite: false,
+        stars: 5.0,
+        imageUrl: 'assets/devils_food.jpg',
+        tags: ['Devils Food', 'Chocolate', 'Peanut', 'Coconut', 'Donut']
+    },
+    {
+        id: '12',
+        name: 'Sprinkle Donut',
+        price: 1.50,
+        favorite: false,
+        stars: 5.0,
+        imageUrl: 'assets/sprinkle_donuts.jpg',
+        tags: ['Sprinkle', 'Chocolate', 'Donut']
+    },
+    {
+        id: '13',
+        name: 'Donut Holes',
+        price: 1.50,
+        favorite: false,
+        stars: 5.0,
+        imageUrl: 'assets/donut_holes.jpg',
+        tags: ['Donut Holes', 'Glazed', 'Dozen']
+    },
+]
+
+export const tags:any[] = [
+    { name: 'All', count: 13 },
+    { name: 'Fancy', count: 4 },
+    { name: 'Apple', count: 2 },
+    { name: 'Cherry', count: 1 },
+    { name: 'Cheese', count: 1 },
+    { name: 'Danish', count: 1 },
+    { name: 'Plain', count: 1 },
+    { name: 'Bar', count: 2 },
+    { name: 'Twist', count: 3 },
+    { name: 'Maple', count: 2 },
+    { name: 'Chocolate', count: 5 },
+    { name: 'Glazed', count: 3 },
+    { name: 'French', count: 1 },
+    { name: 'Devils Food', count: 1 },
+    { name: 'Peanut', count: 1 },
+    { name: 'Coconut', count: 1 },
+    { name: 'Sprinkle', count: 1 },
+    { name: 'Donut', count: 2 },
+    { name: 'Donut Holes', count: 1 },
+    { name: 'Dozen', count: 1 },
+]
