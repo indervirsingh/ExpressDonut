17ee3eb61d2cab93c2dec35bdba387da4c6936ea
Indervir Singh <iindervirsingh@gmail.com>
2023-09-26 13:56:14 -0700

    setting up server on port 5000

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,2 +1,19 @@
 import express from 'express'
 import cors from 'cors'
+
+const app = express()
+
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:4200']
+}))
+
+app.get('/api/foods', (req, res) => {
+    res.send("hello")
+})
+
+const port = 5000
+app.listen(port, () => {
+    console.log("Website served on http://localhost:" + port);
+
+})
