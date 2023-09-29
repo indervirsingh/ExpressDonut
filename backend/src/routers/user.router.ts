cc55f1acddcfe00cb3d0e2351f8f4fe880800c9d
Indervir Singh <iindervirsingh@gmail.com>
2023-09-29 13:39:38 -0700

    user router login works

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -4,6 +4,7 @@ import jwt from 'jsonwebtoken'
 import asyncHandler from 'express-async-handler'
 import { UserModel } from '../models/user.model'
 
+
 const router = Router()
 
 router.get('/seed', asyncHandler(
@@ -19,26 +20,34 @@ router.get('/seed', asyncHandler(
     })
 )
 
-router.post('/login', (req, res) => {
-    const { email, password } = req.body
-    const user = sample_users.find(user => user.email === email && user.password === password)
-
-    if (user) {
-        res.send(generateTokenResponse(user))
-    } else {
-        res.status(400).send("Username or Password is invalid!")
-    }
-})
+router.post('/login', asyncHandler(
+    async (req, res) => {
+        const { email, password } = req.body
+        const user = await UserModel.findOne({email, password})        
+        if (user) {
+            res.send(generateTokenResponse(user))
+        } else {
+            const BAD_REQUEST = 400
+            res.status(BAD_REQUEST).send("Username or Password is invalid!")
+        }
+    })
+)
 
 const generateTokenResponse = (user: any) => {
     const token = jwt.sign({
-        email: user.email, isAdmin: user.isAdmin
+        id: user.id, email: user.email, isAdmin: user.isAdmin
     }, "SomeRandomText", {
         expiresIn: "30d"
     })
 
-    user.token = token
-    return user
+    return {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        address: user.address,
+        isAdmin: user.isAdmin,
+        token: token
+    }
 }
 
 export default router
