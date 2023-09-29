3e3f143c4284718689fc25513bd9eec95269f9b3
Indervir Singh <iindervirsingh@gmail.com>
2023-09-29 12:02:56 -0700

    seeding food to database

--- backend/src/routers/food.router.ts
+++ backend/src/routers/food.router.ts
@@ -1,9 +1,25 @@
 import { Router } from 'express'
 import { foods, tags } from '../data'
+import asyncHandler from 'express-async-handler'
+import { FoodModel } from '../models/food.model'
 
 const router = Router()
 
-router.get('', (req, res) => {
+router.get('/seed', asyncHandler(
+    async(req, res) => {
+        const foodsCount = await FoodModel.countDocuments()
+        if (foodsCount > 0) {
+            res.send("Seed is already done")
+            return
+        }
+
+        await FoodModel.create(foods)
+        res.send("Seed is done")
+    })
+)
+
+
+router.get('/', (req, res) => {
     res.send(foods)
 })
 
