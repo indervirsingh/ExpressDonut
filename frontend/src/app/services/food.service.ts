e277da4c77e7ba2c8e5b3cab527423609aecef99
Indervir Singh <iindervirsingh@gmail.com>
2023-09-29 17:57:22 -0700

    moved constants to shared folder

--- frontend/src/app/services/food.service.ts
+++ frontend/src/app/services/food.service.ts
@@ -3,7 +3,7 @@ import { Tag } from '../shared/models/tag.model'
 import { Food } from '../shared/models/food.model'
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs'
-import { FOODS_BY_ID_URL, FOODS_BY_SEARCH_URL, FOODS_BY_TAG_URL, FOODS_TAGS_URL, FOODS_URL } from '../shared/models/constants/urls'
+import { FOODS_BY_ID_URL, FOODS_BY_SEARCH_URL, FOODS_BY_TAG_URL, FOODS_TAGS_URL, FOODS_URL } from '../shared/constants/urls'
 
 @Injectable({
   providedIn: 'root'
