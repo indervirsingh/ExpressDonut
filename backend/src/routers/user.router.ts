37ed724d3604eb9534f678074f75ba3c6f17d637
Indervir Singh <iindervirsingh@gmail.com>
2023-10-01 16:07:46 -0700

    added check for password validation

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -26,7 +26,7 @@ router.post('/login', asyncHandler(
     async (req, res) => {
         const { email, password } = req.body
         const user = await UserModel.findOne({ email })        
-        if (user) {
+        if (user && (await bcrypt.compare(password, user.password))) {
             res.send(generateTokenResponse(user))
         } else {
             res.status(HTTP_BAD_REQUEST).send("Username or Password is invalid!")
