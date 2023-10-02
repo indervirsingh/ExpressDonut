1e2f71e41581cef8c40eca86980a4924351782b2
Indervir Singh <iindervirsingh@gmail.com>
2023-10-02 12:37:46 -0700

    finished updating

--- backend/src/models/order.model.ts
+++ backend/src/models/order.model.ts
@@ -36,6 +36,31 @@ export interface Order {
     address: string
     addressLatLng: LatLng
     paymentId: string
-    createdAt: string
     status: OrderStatus
+    user: Types.ObjectId
+    createdAt: Date
+    updatedAt: Date
 }
+
+const orderSchema = new Schema<Order>(
+    {
+        name: { type: String, required: true },
+        address: { type: String, required: true },
+        addressLatLng: { type: LatLngSchema, required: true },
+        paymentId: { type: String },
+        totalPrice: { type: Number, required: true },
+        items: { type: [OrderItemSchema], required: true },
+        status: { type: String, default: OrderStatus.new },
+        user: { type: Schema.Types.ObjectId, required: true }
+    }, {
+        timestamps: true,
+        toJSON: {
+            virtuals: true
+        },
+        toObject: {
+            virtuals: true
+        }
+    }
+)
+
+export const OrderModel = model('order', orderSchema)
