5d394ed5a0d597af592d21605af8d6a1128130d4
Indervir Singh <iindervirsingh@gmail.com>
2023-10-02 15:27:58 -0700

    updating router

--- backend/src/routers/order.router.ts
+++ backend/src/routers/order.router.ts
@@ -33,11 +33,14 @@ router.post('/create', asyncHandler(
 
 router.get('/newOrderForCurrentUser', asyncHandler(
     async (req: any, res) => {
-        const order = await OrderModel.findOne({ user: req.user.id, status: OrderStatus.NEW })
+        const order = await getNewOrderForCurrentUser(req)
 
         if (order) res.send(order)
         else res.status(HTTP_BAD_REQUEST).send()
     })
 )
-
 export default router
+
+async function getNewOrderForCurrentUser(req: any) {
+    return await OrderModel.findOne({ user: req.user.id, status: OrderStatus.NEW });
+}
