b8e75a8dd85d0f20426a31ca7e9cba942122046d
Indervir Singh <iindervirsingh@gmail.com>
2023-10-02 16:17:12 -0700

    generated order-track-page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -29,7 +29,8 @@ import { OrderItemsListComponent } from './components/partials/order-items-list/
 import { MapComponent } from './components/partials/map/map.component'
 import { AuthInterceptor } from './auth/auth.interceptor';
 import { PaymentPageComponent } from './components/pages/payment-page/payment-page.component';
-import { PaypalButtonComponent } from './components/partials/paypal-button/paypal-button.component'
+import { PaypalButtonComponent } from './components/partials/paypal-button/paypal-button.component';
+import { OrderTrackPageComponent } from './components/pages/order-track-page/order-track-page.component'
 
 @NgModule({
   declarations: [
@@ -54,7 +55,8 @@ import { PaypalButtonComponent } from './components/partials/paypal-button/paypa
     OrderItemsListComponent,
     MapComponent,
     PaymentPageComponent,
-    PaypalButtonComponent
+    PaypalButtonComponent,
+    OrderTrackPageComponent
   ],
   imports: [
     BrowserModule,
