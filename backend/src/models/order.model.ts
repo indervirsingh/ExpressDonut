98adb1629c5e20479a6624e6031430d479f4be3f
Indervir Singh <iindervirsingh@gmail.com>
2023-10-02 12:25:47 -0700

    implementing OrderItemSchema in order model

--- backend/src/models/order.model.ts
+++ backend/src/models/order.model.ts
@@ -1,4 +1,5 @@
 import { model, Schema, Types } from 'mongoose'
+import { Food, FoodSchema } from './food.model'
 
 export interface LatLng {
     lat: string
@@ -9,5 +10,19 @@ export const LatLngSchema = new Schema<LatLng>(
     {
         lat: { type: String, required: true },
         lng: { type: String, required: true }
+    }
+)
+
+export interface OrderItem {
+    food: Food
+    price: number
+    quantity: number
+}
+
+export const OrderItemSchema = new Schema<OrderItem>(
+    {
+        food: { type: FoodSchema, required: true },
+        price: { type: Number, required: true },
+        quantity: { type: Number, required: true }
     }
 )
