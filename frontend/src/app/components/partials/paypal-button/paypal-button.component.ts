10f0c60fd42ce8eaa7b7c617d4be75233ab5e773
Indervir Singh <iindervirsingh@gmail.com>
2023-10-02 15:34:00 -0700

    removed app from selector

--- frontend/src/app/components/partials/paypal-button/paypal-button.component.ts
+++ frontend/src/app/components/partials/paypal-button/paypal-button.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-paypal-button',
+  selector: 'paypal-button',
   templateUrl: './paypal-button.component.html',
   styleUrls: ['./paypal-button.component.css']
 })
