ff21fbd6d49042de46d074be5da8c0efb5f5c103
Indervir Singh <iindervirsingh@gmail.com>
2023-10-09 11:32:22 -0700

    refactoring

--- backend/src/routes/order.routes.ts
+++ backend/src/routes/order.routes.ts
@@ -1,19 +1,19 @@
 import { Router } from "express"
 import auth from '../middlewares/auth.mid'
-const { getOrder, getOrders, getCurrentOrder, createOrder, pay } = require('../controllers/order.controller')
+const order_controller  = require('../controllers/order.controller')
 
 const router = Router()
 router.use(auth)
 
-router.get('/', getOrders)
+router.get('/', order_controller.getOrders)
 
-router.post('/create', createOrder)
+router.post('/create', order_controller.createOrder)
 
-router.get('/newOrderForCurrentUser', getCurrentOrder)
+router.get('/newOrderForCurrentUser', order_controller.getCurrentOrder)
 
-router.post('/pay', pay)
+router.post('/pay', order_controller.pay)
 
-router.get('/track/:id', getOrder)
+router.get('/track/:id', order_controller.getOrder)
 
 export default router
 
