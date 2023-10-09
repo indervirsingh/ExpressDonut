5f4098bb8c5e91bb3a30b8add2248dcc7864ac09
Indervir Singh <iindervirsingh@gmail.com>
2023-10-09 09:47:31 -0700

    updating routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,9 +4,9 @@ dotenv.config()
 import path from 'path'
 import express from 'express'
 import cors from 'cors'
-import foodRouter from './routes/food.routes'
-import userRouter from './routes/user.routes'
-import orderRouter from './routes/order.routes'
+const food_routes = require('./routes/food.routes')
+const user_routes = require('./routes/user.routes')
+const order_routes = require('./routes/order.routes')
 import { dbConnect } from './configs/database.config'
 
 dbConnect()
@@ -19,9 +19,9 @@ app.use(cors({
     origin: ['http://localhost:4200']
 }))
 
-app.use('/api/foods', foodRouter)
-app.use('/api/users', userRouter)
-app.use('/api/orders', orderRouter)
+app.use('/api/foods', food_routes)
+app.use('/api/users', user_routes)
+app.use('/api/orders', order_routes)
 
 
 app.use(express.static('public'));
