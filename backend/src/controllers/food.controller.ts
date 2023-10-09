36a3547ba40c2072491df1b046267bdb3b2e6899
Indervir Singh <iindervirsingh@gmail.com>
2023-10-09 11:27:23 -0700

    refactoring

--- backend/src/controllers/food.controller.ts
+++ backend/src/controllers/food.controller.ts
@@ -1,71 +1,57 @@
 import asyncHandler from "express-async-handler"
 import { FoodModel } from "../models/food.model"
 
-function getFoods(req: any, res: any) {
-    asyncHandler( async (req, res) => {
-        const foods = await FoodModel.find()
-        res.send(foods)
-    })
-}
+exports.getFoods = asyncHandler( async (req, res) => {
+    const foods = await FoodModel.find()
+    res.send(foods)
+})
 
-function getFood(req: any, res: any) {
-    asyncHandler( async (req, res) => {
-        const foodId = req.params.foodId
-        const food = await FoodModel.findById(foodId)
-        res.send(food)
-    })
-}
+exports.getFood = asyncHandler( async (req, res) => {
+    const foodId = req.params.foodId
+    const food = await FoodModel.findById(foodId)
+    res.send(food)
+})
 
-function getSearch(req: any, res: any) {
-    asyncHandler( async (req, res) => {
-        const searchRegex = new RegExp(req.params.searchTerm, 'i')
-        const foods = await FoodModel.find({ name: {$regex: searchRegex }})
-        res.send(foods)
-    })
-}
 
-function getTags(req: any, res: any) {
-    asyncHandler( async (req, res) => {
-        // 2 foods 3 tags, unwind tags => 6 foods tags 1
-        const tags = await FoodModel.aggregate([
-            {
-                $unwind: '$tags'
-            },
-            {
-                $group: {
-                    _id: '$tags',
-                    count: {$sum: 1}
-                }
-            },
-            {
-                $project: {
-                    _id: 0,
-                    name: '$_id',
-                    count: '$count'
-                }
+exports.getSearch = asyncHandler( async (req, res) => {
+    const searchRegex = new RegExp(req.params.searchTerm, 'i')
+    const foods = await FoodModel.find({ name: {$regex: searchRegex }})
+    res.send(foods)
+})
+
+
+exports.getTags = asyncHandler( async (req, res) => {
+    // 2 foods 3 tags, unwind tags => 6 foods tags 1
+    const tags = await FoodModel.aggregate([
+        {
+            $unwind: '$tags'
+        },
+        {
+            $group: {
+                _id: '$tags',
+                count: {$sum: 1}
             }
-        ]).sort({ count: -1 }) // Ascending
-
-        const all = {
-            name: 'All',
-            count: await FoodModel.countDocuments()
+        },
+        {
+            $project: {
+                _id: 0,
+                name: '$_id',
+                count: '$count'
+            }
         }
+    ]).sort({ count: -1 }) // Ascending
 
-        tags.unshift(all) // Added to the beginning of tags
-        res.send(tags)
-    })
-}
+    const all = {
+        name: 'All',
+        count: await FoodModel.countDocuments()
+    }
 
-function getTag(req: any, res: any) {
-    asyncHandler( async (req, res) => {
-        const foods = await FoodModel.find({tags: req.params.tagName })
-        res.send(foods)
-    })
-}
+    tags.unshift(all) // Added to the beginning of tags
+    res.send(tags)
+})
 
-module.exports = {
-    getFoods,
-    getFood,
-    getSearch,
-    getTags,
-}
+
+exports.getTag = asyncHandler( async (req, res) => {
+    const foods = await FoodModel.find({tags: req.params.tagName })
+    res.send(foods)
+})
