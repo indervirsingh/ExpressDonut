4721a2b7d0c40eef227609c9c7b5cd4a41a2e9b4
Indervir Singh <iindervirsingh@gmail.com>
2023-10-09 11:23:07 -0700

    implemented crud methods

---
+++ backend/src/controllers/user.controller.ts
@@ -0,0 +1,65 @@
+import jwt from 'jsonwebtoken'
+import asyncHandler from 'express-async-handler'
+import { User, UserModel } from '../models/user.model'
+import { HTTP_BAD_REQUEST } from '../constants/http_status'
+import bcrypt from 'bcryptjs'
+
+function login(req: any, res: any) {
+    asyncHandler( async (req, res) => {
+        const { email, password } = req.body
+        const user = await UserModel.findOne({ email })
+        if (user && (await bcrypt.compare(password, user.password))) {
+            res.send(generateTokenResponse(user))
+        } else {
+            res.status(HTTP_BAD_REQUEST).send("Username or Password is invalid!")
+        }
+    })
+}
+
+function register(req: any, res: any) {
+    asyncHandler( async (req: any, res: any) => {
+        
+        const { name, email, password, address } = req.body
+        const user = await UserModel.findOne({ email })
+        if (user && (await bcrypt.compare(password, user.password))) {
+            res.status(HTTP_BAD_REQUEST)
+            .send('User already exists, please login!')
+            return
+        }
+    
+        const encryptedPassword = await bcrypt.hash(password, 10)
+    
+        const newUser: User = {
+            name,
+            email: email.toLowerCase(),
+            password: encryptedPassword,
+            address,
+            isAdmin: false
+        }
+    
+        const dbUser = await UserModel.create(newUser)
+        res.send(generateTokenResponse(dbUser))
+    })
+}
+
+const generateTokenResponse = (user: any) => {
+    const token = jwt.sign({
+        id: user.id, email: user.email, isAdmin: user.isAdmin
+    }, process.env.JWT_SECRET!, {
+        expiresIn: "30d"
+    })
+
+    return {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        address: user.address,
+        isAdmin: user.isAdmin,
+        token: token
+    }
+}
+
+module.exports  = {
+    login,
+    register
+}
