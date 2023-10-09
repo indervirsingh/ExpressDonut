6bb77d8affbb7238e341309bcc884af420279fb4
Indervir Singh <iindervirsingh@gmail.com>
2023-10-09 11:23:14 -0700

    updating routes to use controllers

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,79 +1,21 @@
 import { Router } from 'express'
-// import { sample_users } from '../data'
-import jwt from 'jsonwebtoken'
-import asyncHandler from 'express-async-handler'
-import { User, UserModel } from '../models/user.model'
-import { HTTP_BAD_REQUEST } from '../constants/http_status'
-import bcrypt from 'bcryptjs'
-
-
+const { login, register } = require('../controllers/user.controller')
 const router = Router()
 
-router.get('/seed', asyncHandler(
-    async(req, res) => {
-        const usersCount = await UserModel.countDocuments()
-        if (usersCount > 0) {
-            res.send("Seed is already done")
-            return
-        }
+// router.get('/seed', asyncHandler(
+//     async(req, res) => {
+//         const usersCount = await UserModel.countDocuments()
+//         if (usersCount > 0) {
+//             res.send("Seed is already done")
+//             return
+//         }
 
-        // await UserModel.create(sample_users)
-        res.send("Seed is done")
-    })
-)
+//         // await UserModel.create(sample_users)
+//         res.send("Seed is done")
+//     })
+// )
 
-router.post('/login', asyncHandler(
-    async (req, res) => {
-        const { email, password } = req.body
-        const user = await UserModel.findOne({ email })
-        if (user && (await bcrypt.compare(password, user.password))) {
-            res.send(generateTokenResponse(user))
-        } else {
-            res.status(HTTP_BAD_REQUEST).send("Username or Password is invalid!")
-        }
-    })
-)
-
-router.post('/register', asyncHandler(
-    async (req, res) => {
-        const { name, email, password, address } = req.body
-        const user = await UserModel.findOne({ email })
-        if (user && (await bcrypt.compare(password, user.password))) {
-            res.status(HTTP_BAD_REQUEST)
-            .send('User already exists, please login!')
-            return
-        }
-
-        const encryptedPassword = await bcrypt.hash(password, 10)
-
-        const newUser: User = {
-            name,
-            email: email.toLowerCase(),
-            password: encryptedPassword,
-            address,
-            isAdmin: false
-        }
-
-        const dbUser = await UserModel.create(newUser)
-        res.send(generateTokenResponse(dbUser))
-    }
-))
-
-const generateTokenResponse = (user: any) => {
-    const token = jwt.sign({
-        id: user.id, email: user.email, isAdmin: user.isAdmin
-    }, process.env.JWT_SECRET!, {
-        expiresIn: "30d"
-    })
-
-    return {
-        id: user.id,
-        email: user.email,
-        name: user.name,
-        address: user.address,
-        isAdmin: user.isAdmin,
-        token: token
-    }
-}
+router.post('/login', login)
+router.post('/register', register)
 
 export default router
