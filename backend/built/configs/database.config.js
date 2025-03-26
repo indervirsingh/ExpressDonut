f2ce5ced567bd9366fcfa6dbf1a0491b1f52c5a3
Indervir Singh <indervirsingh18@icloud.com>
2025-03-26 14:02:47 -0700

    refactoring

--- backend/built/configs/database.config.js
+++ backend/built/configs/database.config.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.dbConnect = void 0;
 var mongoose_1 = require("mongoose");
 var dbConnect = function () {
-    (0, mongoose_1.connect)(process.env.MONGO_URI || 'mongodb+srv://indervirsingh:JIG5I8Ok12gRfREC@emails.gfxdggc.mongodb.net/?retryWrites=true&w=majority', {
+    (0, mongoose_1.connect)(process.env.MONGO_URI, {
     }).then(function () { return console.log("connected successfully"); }, function (error) { return console.log(error); });
 };
 exports.dbConnect = dbConnect;
