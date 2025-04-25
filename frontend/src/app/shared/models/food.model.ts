deb2c57396aa1d7e1b547848cf19dd9354387c72
Indervir Singh <indervirsingh18@icloud.com>
2025-04-24 23:08:20 -0700

    Directory is no longer being pushed to github, remains local

--- frontend/src/app/shared/models/food.model.ts
+++
@@ -1,9 +0,0 @@
-export class Food {
-    id!: string
-    name!: string
-    price!: number
-    favorite!: boolean
-    stars!: number
-    imageUrl!: string
-    tags?: string[]
-}
--- frontend/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'https://expressdonut.onrender.com',
-    apiPort: 5100
-}
